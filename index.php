b48fd6926448342d3a08657d9920d82cfe3e8cb7
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-10-01 17:12:03 +0200

    Updated index & style

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 
 require (__DIR__ . "/components/header.php");
+require (__DIR__ . "/components/data.php");
 
 ?>
+
+    <div class="boxes">
+        <?php
+            foreach ($teams as $key => $value) {
+            ?> 
+            <div class="box"> 
+                <a href=""></a>
+                <img src="<?= $value['logo']; ?>" alt="<?= $key; ?>">
+                <h4> <?= $key; ?> </h4>
+            </div><br>
+            <?php
+            }    
+        
 
