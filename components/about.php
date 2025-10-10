b537da8e9b99a605d6e57b9a8fd7487e00fc1f1b
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-10-10 13:21:01 +0200

    Updated: About & Added: about.css

--- components/about.php
+++ components/about.php
@@ -9,15 +9,37 @@ require (__DIR__ . "/header.php");
         <div class="about-img-container">
             <img class="about-img" src="/image/IMG_1090.jpg" alt="image on creator">
         </div>
-        <div class="about-header">
-            <h2>About creator</h2><br>
-        </div>
         <div class="about-text">
+            <h2 class="about-header">About creator</h2><br>
             <p>Hi! My name is Nathalie Rosenkvist and I’m a 27-year-old web development student at Yrgo in Gothenburg.</P> <br>
             <p>I’m currently studying to become a full-stack developer and I’m passionate about clean design, functional code, user-friendly websites and always learning something new. </p><br>
             <p> What I enjoy most about web development is the perfect mix between creativity, logic and designing beautiful interfaces while also solving technical challenges behind the scenes.
             When I’m not coding, you’ll often find me at the gym or playing games or enjoying time outdoors with friends and family. </p><br>
             <p>This website is part of my studies at Yrgo, where I get to combine my interests in technology, design, problem solving while building real-world projects.</p>
+        </div>
+
+        <!-- Cards stars -->
+        <div class="cards">
+            <div class="card">
+                <h2>An amazing office space</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                <div class="card-arrow"></div>
+            </div>
+            <div class="card">
+                <h2>Have you seen my cat?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+                <div class="card-arrow"></div>
+            </div>
+            <div class="card">
+                <h2>Win 5 million dollars!</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmodLorem ipsum dolor sit amet, cons</p>
+                <div class="card-arrow"></div>
+            </div>
+            <div class="card">
+                <h2>Developers! Look at this.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmodLorem ipsum dolor sit amet, cons</p>
+                <div class="card-arrow"></div>
+            </div>
         </div>
     </div>
 
