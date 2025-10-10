207e964f051790b3b4e9fa031bc991d773daa453
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-10-10 10:01:58 +0200

    Updated: About with <p> text

--- components/about.php
+++ components/about.php
@@ -3,17 +3,21 @@ require (__DIR__ . "/header.php");
 ?>
 
     <div class="about-title">
-        <h1>About</h1><br>
+        <h2>About</h2><br>
     </div>
     <div class="about-container">
-        <div class="about-img">
-            <img src="" alt="image on creator">
+        <div class="about-img-container">
+            <img class="about-img" src="/image/IMG_1090.jpg" alt="image on creator">
         </div>
         <div class="about-header">
             <h2>About creator</h2><br>
         </div>
         <div class="about-text">
-            <p></p>
+            <p>Hi! My name is Nathalie Rosenkvist and I’m a 27-year-old web development student at Yrgo in Gothenburg.</P> <br>
+            <p>I’m currently studying to become a full-stack developer and I’m passionate about clean design, functional code, user-friendly websites and always learning something new. </p><br>
+            <p> What I enjoy most about web development is the perfect mix between creativity, logic and designing beautiful interfaces while also solving technical challenges behind the scenes.
+            When I’m not coding, you’ll often find me at the gym or playing games or enjoying time outdoors with friends and family. </p><br>
+            <p>This website is part of my studies at Yrgo, where I get to combine my interests in technology, design, problem solving while building real-world projects.</p>
         </div>
     </div>
 
